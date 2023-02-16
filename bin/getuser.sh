fd1e374823cfb622433b857cf16d53f2bc31d110
Masataka Marukawa <masataka.marukawa@oracle.com>
2023-02-16 17:20:04 +0900

    Update getuser.sh

--- bin/getuser.sh
+++ bin/getuser.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #set -eu
 
-source ./config
 CMDNAME=`basename $0`
-domain=oracle.com
+source ./config
 
 ### Usage ###
 _usage() {
-  echo "usage: ${CMDNAME} [-v] mgrname"
-  echo "    mgrname : Input manager's short name so that all members under him(her) will be eported"
+  echo "usage: ${CMDNAME} [-v] @mgrname | membername"
+  echo "    @mgrname   : a manager's short name with the prefix of "@" to export all members under him(her)."
+  echo "    membername : a member's short name to export individual personnel."
   exit 1
 }
 
@@ -28,89 +28,78 @@ shift `expr ${OPTIND} - 1`
 [ $# -ne 1 ] && _usage
 
 ### Set varibles ###
-toplevel=cn=$1_org_ww
 verbose=${FLG_v}
+input=$1
 
 ### Function : getldapinfo() ###
 function getldapinfo () {
-  group=$1
-  let org_level++
+  local _members
+  local _member
 
-  echo "[${CMDNAME}] exporting members under ${group} organization"
-  members=$(ldapsearch -x -h ${ldapserver} -LLL "${group}" | grep '^uniquemember:' | sed -e 's/uniquemember: //' | sed -e 's/,.*//')
+  if [ ${1:0:1} = "@" ] ; then
+    # if the input is a manager, extract members under the manager group and put them into _members
+    group=cn=${1##@}_org_ww
+    echo "[${CMDNAME}] exporting members under ${group} organization"
+    _members=$(ldapsearch -x -h ${ldapserver} -LLL "${group}" | grep '^uniquemember:' | sed -e 's/uniquemember: //' | sed -e 's/,.*//')
 
-  for member in ${members}; do
-    echo "[${CMDNAME}] collecting ldap information for ${member} ..."
+  else
+    # if the input is a member, then put it into _members
+    _members=uid=${input}
+  fi 
 
-    displayname=$(ldapsearch -x -h $ldapserver -LLL "${member}" displayname | grep '^displayname:' |  sed -e 's/displayname: //')
-    firstname=$(echo ${displayname} | sed -e 's/ .*$//')
-    lastname=$(echo ${displayname} | sed -e 's/^.* //')
-    # uid=$(ldapsearch -x -h $ldapserver -LLL "${member}" uid | grep '^uid:' |  sed -e 's/uid: //' | tr '[:upper:]' '[:lower:]')
+  for _member in ${_members}; do
+    
+    if [ "$(echo ${_member} | grep _org_ww)" ] ; then
+      echo "[${CMDNAME}] ${_member} is a nested group. extracting..."  1>&2
+      getldapinfo @$(echo $_member | sed -e 's/^cn=//' | sed -e 's/_org_ww$//')
 
-    email=$(ldapsearch -x -h $ldapserver -LLL "${member}" mail | grep '^mail:' |  sed -e 's/mail: //')
-    uname=$(echo $email | sed -e 's/@'${domain}'//' | tr '[:upper:]' '[:lower:]')
-
-    if [ "$(echo ${uname} | grep _org_ww)" ] ; then
-      echo "[${CMDNAME}] ${uname} is a group. skipped."  1>&2
     else
+      echo "[${CMDNAME}] collecting ldap information for ${_member} ..."
+
+      displayname=$(ldapsearch -x -h $ldapserver -LLL "${_member}" displayname | grep '^displayname:' |  sed -e 's/displayname: //')
+      firstname=$(echo ${displayname} | sed -e 's/ .*$//')
+      lastname=$(echo ${displayname} | sed -e 's/^.* //')
+      email=$(ldapsearch -x -h $ldapserver -LLL "${_member}" mail | grep '^mail:' |  sed -e 's/mail: //')
+      uname=$(echo $email | sed -e 's/@'${domain}'//' | tr '[:upper:]' '[:lower:]')
+
       echo "${email},${email},,work,,${firstname},,${lastname},"  >>${idcsusers}.new
       echo "oci_member-${uname}_admins,,${email}"  >>${idcsgroups}.new
-      echo "Normal Users,,${email}"  >>${idcsgroups}.new
-
-      echo "    - user name   : ${uname}"  1>&2
-      echo -n "  {name=\"${uname}\", org={"  >>${input}.new
-      org[${org_level}]=$(echo ${group} | sed -e 's/cn=//' | sed -e 's/_ww//')  
-
-      cnt=1
-      while [ ${cnt} -le ${org_level} ] ; do
-        echo "    - org_level_${cnt} : ${org[${cnt}]}"  1>&2
-        echo -n "\"fy20_level_${cnt}\"=\"${org[${cnt}]}\""  >>${input}.new
-
-        if [ ${cnt} -lt ${org_level} ] ; then
-          echo -n ", "  >>${input}.new
-        fi
-
-        let cnt++
-      done
-
-      echo "}},"  >>${input}.new
+      echo "Regular_Users,,${email}"  >>${idcsgroups}.new
+      echo "  {name=\"${uname}\"},"  >>${terraform}.new
     fi
-
     done
-
-  nestedgroups=$(ldapsearch -x -h ${ldapserver} -LLL "${group}" | grep '^uniquemember:' | grep 'cn=org_groups' | sed -e 's/uniquemember: //' | sed -e 's/,.*//')
-
-  for group in ${nestedgroups}; do
-    getldapinfo $group
-  done
-
-  let org_level--
 
   return 0
 }
 
 
 ### Main ###
-echo "[${CMDNAME}] start exporting all the members under ${toplevel} from ldap server" 1>&2
+echo "[${CMDNAME}] start exporting all the members under ${input} from ldap server" 1>&2
 echo "    - ldap server : ${ldapserver}" 1>&2
 echo "    - domain      : ${domain}" 1>&2
 
-input=users_$1.tfvars
-idcsusers=users_idcs_$1.csv
-idcsgroups=groups_idcs_$1.csv
-rm -f ${input}.new {idcsusers}.new {idcsgroups}.new
+terraform=users_${input}.tfvars
+idcsusers=users_idcs_${input}.csv
+idcsgroups=groups_idcs_${input}.csv
 
+# Create blank files
+rm -f ${terraform}.new {idcsusers}.new {idcsgroups}.new
+## Terraform tfvars file
+echo "users = ["  >>${terraform}.new
+## IDCS users csv file
 echo "User Name,Work Email,Home Email,Primary Email Type,Honorific Prefix,First Name,Middle Name,Last Name,Honorific Suffix"  >>${idcsusers}.new
+## IDCS groups csv file
 echo "Name,Description,User Members"  >>${idcsgroups}.new
-echo "users = ["  >>${input}.new
-org_level=0
-org=()
-getldapinfo $toplevel
-echo "]"  >>${input}.new
 
-echo "[${CMDNAME}] export completed. exported file : ${idcsusers}, ${idcsgroups}, ${input}" 1>&2
-rm -f ${input} ${idcsusers} ${idcsgroups}
+# Get user data from ldap and add lines to the tfvars/csv files
+getldapinfo ${input}
+
+# Add surfix data
+echo "]"  >>${terraform}.new
+
+# Closing
+echo "[${CMDNAME}] export completed. exported file : ${idcsusers}, ${idcsgroups}, ${terraform}" 1>&2
+rm -f ${terraform} ${idcsusers} ${idcsgroups}
 mv ${idcsusers}.new ${idcsusers}
 mv ${idcsgroups}.new ${idcsgroups}
-mv ${input}.new ${input}
-
+mv ${terraform}.new ${terraform}
